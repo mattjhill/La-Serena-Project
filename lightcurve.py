76dc48b0a89acf357db1776ce28429de2302a670
Shiyuan <shiyuanhe@Shiyuans-MacBook-Pro.local>
2015-08-19 16:35:27 -0400

    lowess clean, sample data and a test file

--- lightcurve.py
+++ lightcurve.py
@@ -1,4 +1,5 @@
 import numpy as np
+import statsmodels.api as sm
 
 class LightCurve(object):
 	""" 
@@ -12,6 +13,10 @@ class LightCurve(object):
 		the mesured magnitude at the time t
 	merr : numpy array
 		the photometric magnitude error for the observation
+
+        outlier : numpy array
+                 Default value False. Set to True if the observation 
+                  is outlier
 	"""
 
 	def __init__(self, fname):
@@ -25,4 +30,12 @@ class LightCurve(object):
 			the filename of the light curve
 		"""
 		self.t, self.m, self.merr = np.loadtxt(fname, unpack=True)
-	
+                self.outlier = np.repeat(False, len(self.t))
+
+        def lowessClean(self, K=5):
+                lowess = sm.nonparametric.lowess
+                z = lowess(self.m, self.t,frac=0.4)
+                residuals = z[:,1]-self.m
+                residSigma = np.std(residuals)
+                self.outlier = np.abs(residuals) > K*residSigma
+                
---
+++ test.py
@@ -0,0 +1,7 @@
+from lightcurve import *
+
+testCurve = LightCurve("2110217026031.dat")
+testCurve.lowessClean(K=2)
+
+
+
