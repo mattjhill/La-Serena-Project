1df73137c9df16835ad4d6efcd23d9988746b351
Matt Hill <mhill92@gmail.com>
2015-08-19 11:15:41 -0300

    added the LightCurve object and __init__ method

--- lightcurve.py
+++ lightcurve.py
@@ -1,1 +1,28 @@
-import numpy
+import numpy as np
+
+class LightCurve(object):
+	""" 
+	The CRTS light curve object 
+
+	Attributes
+	----------
+	t : numpy array
+		the Modified Julian Dates, i.e. the time of observation
+	m : numpy array
+		the mesured magnitude at the time t
+	merr : numpy array
+		the photometric magnitude error for the observation
+	"""
+
+	def __init__(self, fname):
+		""" 
+		The initilization function. It creates an object from
+		the actual CRTS data file.
+
+		Parameters
+		----------
+		fname : string
+			the filename of the light curve
+		"""
+		self.t, self.m, self.merr = np.loadtxt(fname, unpack=True)
+	
