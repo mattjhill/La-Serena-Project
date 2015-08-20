708b6400698ae3763cf971a97b324d7ea2e09349
Matt Hill <mhill92@gmail.com>
2015-08-20 10:50:05 -0300

    added period searching

--- lightcurve.py
+++ lightcurve.py
@@ -1,6 +1,9 @@
 import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+from gatspy.periodic import LombScargleFast
+import pyaov
+
 
 class LightCurve(object):
 	""" 
@@ -31,20 +34,51 @@ class LightCurve(object):
 			the filename of the light curve
 		"""
 		self.t, self.m, self.merr = np.loadtxt(fname, unpack=True)
-                self.outlier = np.repeat(False, len(self.t))
+        self.outlier = np.repeat(False, len(self.t))
 
-        def lowessClean(self, K=5):
-                lowess = sm.nonparametric.lowess
-                z = lowess(self.m, self.t,frac=0.4)
-                residuals = z[:,1]-self.m
-                residSigma = np.std(residuals)
-                self.outlier = np.abs(residuals) > K*residSigma
+	def lombscargle(self):
+		"""
+		Use gatpsy for fast computing of the Lomb-Scargle periodogram.
+		"""
+		model = LombScargleFast().fit(self.t, self.m, self.merr)
+		self.ls_periods, self.ls_power = model.periodogram_auto(nyquist_factor=200)
 
-        # plot observation
-        def plotObs(self):
-                plt.plot(self.t[self.outlier==True], self.m[self.outlier==True],
-                         'o',color='0.75',mec='none')
-                plt.plot(self.t[self.outlier==False],self.m[self.outlier==False],
-                         'ko')
-                plt.errorbar(self.t, self.m, self.merr,fmt='none',ecolor="0.8")
-                plt.savefig("lightcurve.png")
+	def aov(self):
+		"""
+		Compute the Analysis of Variance (AOV) periodogram. Use the same frequency grid as LS.
+		"""
+		fstop = max(1/self.ls_periods)
+		fstep = np.diff(1/self.ls_periods)[0]
+		fr0 = min(1/self.ls_periods)
+		self.aov, self.fr, self.aov_Fbest = pyaov.aovw(self.t, self.m, self.merr, fstop=fstop, fstep=fstep, fr0=fr0)
+
+	def psearch(self):
+		"""
+		Convole the two periodograms and find the Max
+		"""
+
+		self.conv_pgram = self.ls_power*self.aov 
+		self.conv_fbest = self.fr[np.argmax(self.conv_pgram)]
+		self.conv_pbest = 1/self.conv_fbest
+
+	def get_pbest(self):
+		self.lombscargle()
+		self.aov()
+		self.psearch()
+		print(self.conv_pbest)
+
+    def lowessClean(self, K=5):
+        lowess = sm.nonparametric.lowess
+        z = lowess(self.m, self.t,frac=0.4)
+        residuals = z[:,1]-self.m
+        residSigma = np.std(residuals)
+        self.outlier = np.abs(residuals) > K*residSigma
+
+    # plot observation
+    def plotObs(self):
+        plt.plot(self.t[self.outlier==True], self.m[self.outlier==True],
+                 'o',color='0.75',mec='none')
+        plt.plot(self.t[self.outlier==False],self.m[self.outlier==False],
+                 'ko')
+        plt.errorbar(self.t, self.m, self.merr,fmt='none',ecolor="0.8")
+        plt.savefig("lightcurve.png")
